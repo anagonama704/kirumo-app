75761787ac906105b21b7aacacf517519320cea3
kei <dr.kei21@gmail.com>
2023-10-13 10:25:14 +0900

    add：詳細ページのパス変更

---
+++ src/app/clothes-list/clothes-detail/[id]/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { Container, Text } from "@mantine/core";
+const ClothesDetail = () => {
+  return (
+    <Container>
+      <Text>商品詳細ページ</Text>
+    </Container>
+  );
+};
+export default ClothesDetail;
