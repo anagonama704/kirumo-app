c96aa3ee79b98ae56649d037cc89571e6494d959
kei <dr.kei21@gmail.com>
2023-10-23 11:14:41 +0900

    update:Tabごとのデータ取得

--- src/app/clothes-list/page.tsx
+++ src/app/clothes-list/page.tsx
@@ -20,42 +20,84 @@ import {
 import classes from "../Demo.module.css";
 import { clotheList, recommend } from "@/mock/mockdata";
 import List from "@/components/ClothesList";
+import { useEffect, useState } from "react";
+import { clotherList } from "../../../api/@types";
+import Recommend from "@/components/Recommend";
+import RecsList from "@/components/RecsList";
 
 const ClothesList = () => {
+  const [val, setVal] = useState("おすすめ");
+  const [sort, setSort] = useState<clotherList[]>(recommend);
   const searchIcon = <MagnifyingGlass size={20} />;
+  const [tabListData, setTabListData] = useState<clotherList[]>(clotheList);
+
+  const [di, setdd] = useState<boolean>(true);
+  const [listDis, setListDis] = useState<boolean>(false);
+  const tabChangeData = (e: string | null) => {
+    setVal(e ? e : "");
+  };
+  const fff = (val: string) => {
+    if (val === "おすすめ") {
+      setListDis(false);
+      setTimeout(() => {
+        setdd(true);
+      }, 1000);
+    } else {
+      setdd(false);
+      setListDis(true);
+      setTimeout(() => {
+        setdd(true);
+      }, 1000);
+      setSort(
+        recommend.filter((sortData) => {
+          return sortData.sort.includes(val);
+        })
+      );
+      setTabListData(
+        clotheList.filter((tabData) => {
+          return tabData.sort.includes(val);
+        })
+      );
+    }
+  };
+  useEffect(() => {
+    fff(val);
+  }, [val]);
   return (
-    <Container fluid>
+    <Container fluid m={0} p={0}>
       <Tabs
-        top={0}
         pos="sticky"
+        top={0}
+        left={0}
         variant="unstyled"
-        defaultValue="rec"
+        value={val}
         classNames={classes}
         style={{ zIndex: 100000, backgroundColor: "#fff" }}
+        onChange={tabChangeData}
+        ml={200}
       >
         <Tabs.List grow>
-          <Tabs.Tab value="1" disabled w={142}></Tabs.Tab>
-          <Tabs.Tab value="rec" leftSection={<ThumbsUp size={32} />}>
+          <Tabs.Tab value="おすすめ" leftSection={<ThumbsUp size={32} />}>
             おすすめ
           </Tabs.Tab>
-          <Tabs.Tab value="out" leftSection={<Hoodie size={32} />}>
+          <Tabs.Tab value="アウター" leftSection={<Hoodie size={32} />}>
             アウター
           </Tabs.Tab>
-          <Tabs.Tab value="top" leftSection={<TShirt size={32} />}>
+          <Tabs.Tab value="トップス" leftSection={<TShirt size={32} />}>
             トップス
           </Tabs.Tab>
-          <Tabs.Tab value="bot" leftSection={<Pants size={32} />}>
+          <Tabs.Tab value="ボトムス" leftSection={<Pants size={32} />}>
             ボトムス
           </Tabs.Tab>
-          <Tabs.Tab value="sho" leftSection={<Sneaker size={32} />}>
+          <Tabs.Tab value="シューズ" leftSection={<Sneaker size={32} />}>
             シューズ
           </Tabs.Tab>
         </Tabs.List>
       </Tabs>
       <Flex>
         <Box
-          w={220}
-          h={"90vh"}
+          w={225}
+          h={"85vh"}
           top={53}
           pos="sticky"
           p={10}
@@ -85,7 +127,13 @@ const ClothesList = () => {
             />
           </Input.Wrapper>
         </Box>
-        <List clotheList={clotheList} recommend={recommend} />
+        <Box style={{ width: "100%", backgroundColor: "#fff" }}>
+          {listDis ? (
+            <Recommend recommend={sort} tys={di} clotheList={tabListData} />
+          ) : (
+            <RecsList recommend={recommend} />
+          )}
+        </Box>
       </Flex>
     </Container>
   );
