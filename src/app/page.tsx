92331a6af59356e5480597c737d5b0ed56d2d2e9
bassa-1791 <z9q2sbfpy8pbpbrm7r3z@icloud.com>
2023-10-30 09:37:34 +0900

    add:トップページの作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,34 @@
 "use client";
 
-import { Container, Text } from "@mantine/core";
+import {
+  Box,
+  Button,
+  Center,
+  Container,
+  Flex,
+  Text,
+  Image,
+  BackgroundImage,
+} from "@mantine/core";
+import topimg4 from "/public/images/topimg4.png";
+import { relative } from "path";
 const Home = () => {
   return (
-    <Container>
-      <Text>トップページ</Text>
+    <Container fluid m={0} p={0}>
+      <Box
+        component={Flex}
+        justify="center"
+        align="center"
+        w={"100%"}
+        h={"100vh"}
+        style={{
+          backgroundImage: "url(" + topimg4.src + ")",
+          backgroundPosition: "0% 100%",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <Text fz={"2.5em"}>カードをかざしてください</Text>
+      </Box>
     </Container>
   );
 };
